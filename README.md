8ded892e7cfb97893fcf03e13c21d8989c3ea12e
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-12-22 21:13:09 -0500

    file env and dependencys injection

---
+++ README.md
@@ -0,0 +1,14 @@
+# Proyecto NOC
+El objetivo es crear una serie de tareas usando Arquitectura Limpia con Typescript
+
+# dev
+1. Clonar el archivo ```.env.template``` y renombrarlo ```.env```
+2. Conigurar las variables de entorno
+```
+PORT=3000
+
+MAILER_EMAIL=
+MAILER_PASSWORD=
+
+PROD=false
+```
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,5 +1,9 @@
 import { CheckService } from "../domain/use-cases/checks/check-service";
+import { FileSystemDatasource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cront-service"
+
+const fileSystemLogRepository = new LogRepositoryImpl(new FileSystemDatasource);
 
 
 export class Server {
@@ -10,8 +14,10 @@ export class Server {
     CronService.createJob(
       '*/5 * * * * *',
       () => {
-        const url = 'https://google.com';
+        // const url = 'https://google.com';
+        const url = 'http://localhost:3000';
         new CheckService(
+          fileSystemLogRepository,
           () => console.log(`${url} is ok`),
           (error) => console.log(error)
         ).execute(`${url}`);
